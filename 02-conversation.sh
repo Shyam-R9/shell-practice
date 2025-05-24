5ebcedfb114b5986b80ed725e8e0393ee13388c6
shyam <shyam9.r9@gmail.com>
2025-05-24 13:07:16 +0530

    conversation started

---
+++ 02-conversation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "RSP: How are you"
+echo "AA: Good, thanks for asking. How are you dong"
+echo "RSP: When are you gong to home town"
+echo "AA: 29th May. What about you"
+echo "RSP: 29th May"
+echo "AA: ok then, lets meet in the railway station."
+echo "RSP: Sure, will be waiting. "
+echo "AA: Me too"
