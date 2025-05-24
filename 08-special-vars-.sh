6233750b70a114186b6d66e7f34d8e19e93678ce
shyam <shyam9.r9@gmail.com>
2025-05-24 16:26:20 +0530

    special variables

--- 08-special-vars-.sh
+++ 08-special-vars-.sh
@@ -2,4 +2,12 @@
 
 echo "all variales passed to the scritp: $@"
 echo "number of variables passed: $#"
-echo " exit code of the last command: $?"
+echo "exit code of the last command: $?"
+echo "scrip name: $0:"
+echo "current directory: $PWD"
+echo "user running the script: $USER"
+echo "home directory of the user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of the last command in background: $!"
+
