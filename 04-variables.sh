abb1a96acb166819a75dcf5baf64786ae5323dd2
shyam <shyam9.r9@gmail.com>
2025-05-24 14:07:15 +0530

    conversation started

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,13 +1,12 @@
+#PERSON1=$1
+#PERSON2=$2
 
-PERSON1=$1
-PERSON2=$2
 
-
-echo "$PERSON1: How are you, $PERSON2"
-echo "$PERSON2: Good $PERSON1, thanks for asking. How are you dong"
-echo "$PERSON1: I am good. When are you gong to home town $PERSON2"
-echo "$PERSON2: 29th May. What about you"
-echo "$PERSON1: 29th May"
-echo "$PERSON2: ok then, lets meet in the railway station."
-echo "$PERSON1: Sure, will be waiting. "
-echo "$PERSON2: Me too"
+echo "$1: How are you, $2"
+echo "$2: Good $1, thanks for asking. How are you dong"
+echo "$1: I am good. When are you gong to home town $2"
+echo "$2: 29th May. What about you"
+echo "$1: 29th May"
+echo "$2: ok then, lets meet in the railway station."
+echo "$1: Sure, will be waiting. "
+echo "$2: Me too"
