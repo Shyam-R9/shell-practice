3abd4c3b518985ce8ac8429c6461cba84cb08d0d
shyam <shyam9.r9@gmail.com>
2025-05-26 14:19:49 +0530

    install commands

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -4,26 +4,8 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "Error: You are not installing with root access"
-    exit 1
+    
 else
     echo "You are installing with root access"
 
 fi
-
-dnf list installed mysql
-
-if [ $? -ne 0 ]; then
-    echo "Installing mysql"
-
-    dnf install mysql -y
-
-    if [ $? -ne 0 ]; then
-        echo "my sql installation failed."
-        exit 1
-    else
-        echo "sql installed successfully" 
-    fi
-else
-    echo "SQL already installed"
-fi
-
