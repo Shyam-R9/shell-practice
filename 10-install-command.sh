df6eeefd7f46dbd20e0a6cb138a6f9201bd7e2e0
shyam <shyam9.r9@gmail.com>
2025-05-27 12:08:49 +0530

    install mysql

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -2,13 +2,31 @@
 
 USERID=$(id -u)
 
-if [ $USERID -eq 0 ]; then
-    echo "You are working with root access"
+if [ "$USERID" -eq 0 ]; then
+    echo "You are installing with root access"
 else
-    echo " you are not a root user"
-    echo " your eid is $(iiid -u)"
-    echo "your elseid is $(id -u)"
+    echo "Error: You need admin access to install mysql"
+    exit 1
 fi
-df
-dnf install mysql
-echo "your id is $(id -u)"
+
+echo "Checking if mysql is already installed"
+
+dnf list installed mysql
+
+if [ $? -eq 0 ]; then
+    echo "MySql is already installed on this system"
+    exit 0
+else
+    echo "MySql is not installed on this system, proceeding to install"
+    
+    dnf install mysql -y
+    
+    if [ $? -eq 0 ]; then
+        echo "MySql installed successfully"
+    else
+        echo "MySQL installation failed"
+
+    fi
+fi
+
+    
