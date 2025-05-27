81a25b0d8416f475b3cc2c86d753f36c152e56c8
shyam <shyam9.r9@gmail.com>
2025-05-27 20:37:06 +0530

    colors

--- 14-logs.sh
+++ 14-logs.sh
@@ -5,6 +5,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOG_FOLDER=/var/log/shellscripts-logs
+mkdir -p $LOG_FOLDER
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
@@ -19,7 +20,7 @@ validate_install() {
     fi    
 }
 
-mkdir -p $LOG_FOLDER
+
 
 USERID=$(id -u)
 
