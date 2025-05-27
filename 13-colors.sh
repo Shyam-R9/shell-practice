4d6c8f68d7843c377892e84c42e03cc5e63c8496
shyam <shyam9.r9@gmail.com>
2025-05-27 17:19:17 +0530

    colors

--- 13-colors.sh
+++ 13-colors.sh
@@ -10,7 +10,7 @@ validate_install() {
     if [ "$1" -eq 0 ]; then
         echo -e "$G $2 installed successfully $N"
     else
-        echo "$R $2 installation failed $N"
+        echo -e "$R $2 installation failed $N"
         exit 1
 
     fi    
@@ -21,7 +21,7 @@ USERID=$(id -u)
 if [ "$USERID" -eq 0 ]; then
     echo "You are running the script with root access"
 else
-    echo "$R custErr:You need admin access to run this script $N"
+    echo -e "$R custErr:You need admin access to run this script $N"
     exit 1
 fi
 
@@ -29,31 +29,31 @@ fi
 dnf list installed mysql
 
 if [ $? -eq 0 ]; then
-    echo "$Y MySql is already installed on this system $N"
+    echo -e "$Y MySql is already installed on this system $N"
 else
     echo "MySql is not installed on this system, proceeding to install"
     dnf install mysql -y
-    VALIDATE $? "mysql"
+    validate_install $? "mysql"
 fi
 
 dnf list installed python3
 
 if [ $? -eq 0 ]; then
-    echo "$Y Python is already installed on this system $N"
+    echo -e "$Y Python is already installed on this system $N"
 else
     echo "Python is not installed on this system, proceeding to install"
     dnf install python3 -y
-    VALIDATE $? "python"
+    validate_install $? "python"
 fi
 
 dnf list installed nginx
 
 if [ $? -eq 0 ]; then
-    echo "$Y nginx is already installed on this system $N"
+    echo -e "$Y nginx is already installed on this system $N"
 else
     echo "nginx is not installed on this system, proceeding to install"
     dnf install nginx -y
-    VALIDATE $? "nginx"
+    validate_install $? "nginx"
 fi
 
 
