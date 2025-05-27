f725b7bbdab49a1109dd2052f3b43e6fd82db437
shyam <shyam9.r9@gmail.com>
2025-05-27 19:49:10 +0530

    colors

--- 14-logs.sh
+++ 14-logs.sh
@@ -9,14 +9,14 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOG_FOLDER
-echo "Script execution started at: $(date)" >> $LOG_FILE
+echo "Script execution started at: $(date)" | tee -a $LOG_FILE
 
 validate_install() {
         
     if [ "$1" -eq 0 ]; then
-        echo -e "$G $2 installed successfully $N" >> $LOG_FILE
+        echo -e "$G $2 installed successfully $N" | tee -a $LOG_FILE
     else
-        echo -e "$R $2 installation failed $N" >> $LOG_FILE
+        echo -e "$R $2 installation failed $N" | tee -a $LOG_FILE
         exit 1
 
     fi    
@@ -25,9 +25,9 @@ validate_install() {
 USERID=$(id -u)
 
 if [ "$USERID" -eq 0 ]; then
-    echo "You are running the script with root access" >> $LOG_FILE
+    echo "You are running the script with root access" | tee -a $LOG_FILE
 else
-    echo -e "$R custErr:You need admin access to run this script $N" >> $LOG_FILE
+    echo -e "$R custErr:You need admin access to run this script $N" | tee -a $LOG_FILE
     exit 1
 fi
 
@@ -35,9 +35,9 @@ fi
 dnf list installed mysql
 
 if [ $? -eq 0 ]; then
-    echo -e "$Y MySql is already installed on this system $N" >> $LOG_FILE
+    echo -e "$Y MySql is already installed on this system $N" | tee -a $LOG_FILE
 else
-    echo "MySql is not installed on this system, proceeding to install" >> $LOG_FILE
+    echo "MySql is not installed on this system, proceeding to install" | tee -a $LOG_FILE
     dnf install mysql -y
     validate_install $? "mysql"
 fi
@@ -45,9 +45,9 @@ fi
 dnf list installed python3
 
 if [ $? -eq 0 ]; then
-    echo -e "$Y Python is already installed on this system $N" >> $LOG_FILE
+    echo -e "$Y Python is already installed on this system $N" | tee -a $LOG_FILE
 else
-    echo "Python is not installed on this system, proceeding to install" >> $LOG_FILE
+    echo "Python is not installed on this system, proceeding to install" | tee -a $LOG_FILE
     dnf install python3 -y
     validate_install $? "python"
 fi
@@ -55,9 +55,9 @@ fi
 dnf list installed nginx
 
 if [ $? -eq 0 ]; then
-    echo -e "$Y nginx is already installed on this system $N" >> $LOG_FILE
+    echo -e "$Y nginx is already installed on this system $N" | tee -a $LOG_FILE
 else
-    echo "nginx is not installed on this system, proceeding to install" >> $LOG_FILE
+    echo "nginx is not installed on this system, proceeding to install" | tee -a $LOG_FILE
     dnf install nginx -y
     validate_install $? "nginx"
 fi
