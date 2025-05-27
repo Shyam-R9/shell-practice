e3484a45fcac1393f11a2446e860bde21a2bf09a
shyam <shyam9.r9@gmail.com>
2025-05-27 19:46:21 +0530

    colors

--- 14-logs.sh
+++ 14-logs.sh
@@ -5,7 +5,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOG_FOLDER=/var/logs/shellscripts-logs
-SCRIPT_NAME=$($0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOG_FOLDER
